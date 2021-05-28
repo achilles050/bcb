3ede44ac1a720569ee6fb1a95f2eb2c885bc18dc
Kamthorn <thorn3579@gmail.com>
2021-05-29 04:29:59 +0700

    edit setting

--- bbc/settings.py
+++ bbc/settings.py
@@ -202,8 +202,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 ALLOWED_HOSTS=['http://127.0.0.1:3000',
                 'http://localhost:3000',
