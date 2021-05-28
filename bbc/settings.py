5e6bcd23ad3ecf56e25fd97341ab7bf987c45251
Kamthorn <thorn3579@gmail.com>
2021-05-29 04:36:26 +0700

    edit setting

--- bbc/settings.py
+++ bbc/settings.py
@@ -202,25 +202,27 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_ALLOW_ALL = False
 
-ALLOWED_HOSTS=['http://127.0.0.1:3000',
-                'http://localhost:3000',
-                'https://kittichat.github.io',
-                '.heroku.com',]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = False
+
+# ALLOWED_HOSTS=['http://127.0.0.1:3000',
+#                 'http://localhost:3000',
+#                 'https://kittichat.github.io',
+#                 '.heroku.com',]
 
 # CSRF_COOKIE_SECURE = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SAMESITE = 'Lax'
 # SESSION_COOKIE_SAMESITE = 'Lax'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'https://kittichat.github.io',
-    '.heroku.com',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:3000',
+#     'http://localhost:3000',
+#     'https://kittichat.github.io',
+#     '.heroku.com',
+# ]
 
 # SESSION_COOKIE_DOMAIN = ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://kittichat.github.io',
 #                         '0.0.0.0']
