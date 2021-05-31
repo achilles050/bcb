8b56c7296ed50a8870741df6a757a720e51f57f3
Kamthorn <thorn3579@gmail.com>
2021-05-31 10:54:46 +0700

    load data to db

--- bbc/settings.py
+++ bbc/settings.py
@@ -114,19 +114,6 @@ WSGI_APPLICATION = 'bbc.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'sql6414819',
-#         'HOST': 'sql6.freemysqlhosting.net',
-#         'USER': 'sql6414819',
-#         'PASSWORD': '1f4WjUCwaS',
-#         'PORT': 3306,
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         },
-#     }
-# }
 
 # DATABASES = {
 
